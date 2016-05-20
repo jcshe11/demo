cd21b891cb22e0fb2a04d6889f3f7d2ec63e3838
yourusername <youremail@student.ait.ie>
2016-05-20 09:19:26 +0100

    first commit

---
+++ winecellar-app/src/main/java/com/winecellar/winecellar_app/rest/WineWS.java
@@ -0,0 +1,83 @@
+package com.winecellar.winecellar_app.rest;
+
+import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.winecellar.winecellar_app.data.WineDAO;
+import com.winecellar.winecellar_app.model.Wine;
+
+@Path("/wines")
+@Stateless
+@LocalBean
+public class WineWS {
+
+	@EJB
+	private WineDAO wineDao;
+	
+	public int getValue(){
+		return 2;
+	}
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response findAllWines() {
+		System.out.println("Get all wines++test");
+		final List<Wine> wines=wineDao.getAllWines();
+		System.out.println("got wines");
+		System.out.println(wines.size());
+		return Response.status(200).entity(wines).build();
+	}
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	@Path("/{id}")
+	public Response findWineById(@PathParam("id") final int identity) {
+		final Wine wine = wineDao.getWine(identity);
+		return Response.status(200).entity(wine).build();
+
+	}
+	
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response saveWine(final Wine wine) {
+		wineDao.save(wine);
+		return Response.status(201).entity(wine).build();
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes("application/json")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response updateWine(final Wine wine) {
+		wineDao.update(wine);
+		return Response.status(200).entity(wine).build();
+	}
+
+	@DELETE
+	@Path("/{id}")
+	public Response deleteCar(@PathParam("id") final int identity) {
+		wineDao.delete(identity);
+		return Response.status(204).build();
+	}
+	
+	@GET
+	@Path("/search/{query}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response findByName(@PathParam("query") final String query) {
+		System.out.println("findByName: " + query);
+		final List<Wine> wines=wineDao.getWinesByName(query);
+		return Response.status(200).entity(wines).build();
+	}
+
+}
